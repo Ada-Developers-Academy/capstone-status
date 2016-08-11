289c1c27e0c37a2fe120bcd39ebeb4323f9a4315
jnf <jeremy.flores@gmail.com>
2016-08-11 13:17:13 -0700

    orgnaizes views and markup into stylable chunks

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,18 +1,26 @@
 class CheckinsController < ApplicationController
+  before_action :setup_nav_data
+
   def students
-    @students = Student.order(:name)
+
   end
 
   def student
-    @students = Student.order(:name)
     @student = Student.includes(:checkins).find(params[:id])
   end
 
   def by_date
-    @dates = Checkin.distinct_dates
+
   end
 
   def date
     @checkins = Checkin.includes(:student).where(collected_at: params[:date])
+  end
+
+  private
+
+  def setup_nav_data
+    @students = Student.order(:name)
+    @dates = Checkin.distinct_dates
   end
 end
